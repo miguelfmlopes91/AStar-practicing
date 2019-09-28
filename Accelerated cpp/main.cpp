d76ff8ea9e1dd39395c668f6a3e4fea89ecf3660
Miguel Lopes <miguel.lopes@miniclip.com>
2019-09-28 19:18:26 +0100

    Initial Commit

---
+++ Accelerated cpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Accelerated cpp
+//
+//  Created by Miguel Lopes on 28/09/2019.
+//  Copyright © 2019 Miguel Lopes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
